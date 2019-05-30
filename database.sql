99ebcc34302ff8cd9454e0ad2fb8da3b410d7062
mattc0llier <matt.collier@hotmail.com>
2019-05-30 17:13:12 +0100

    put initial objectives in database - not quite working

--- database.sql
+++ database.sql
@@ -5,4 +5,31 @@ DROP DATABASE encode;
 CREATE DATABASE encode;
 
 -- If you prefer to simply delete the tables instead of the whole database, user the code below:
-DROP TABLE example;
+DROP TABLE objectives;
+DROP TABLE lessons;
+
+-- Insert this code inside your better_change database
+CREATE TABLE objectives(
+  id SERIAL PRIMARY KEY,
+  number INT,
+  objective VARCHAR(1000) NOT NULL,
+  url VARCHAR(1000) NOT NULL,
+  lesson_id INT
+);
+
+CREATE TABLE lessons(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(500) NOT NULL,
+  url VARCHAR(1000) NOT NULL,
+  course_id INT
+);
+
+
+INSERT INTO objectives
+  (id, number, objective, url, lesson_id)
+  VALUES
+  (1, 1, 'Learn to “think like a Software Engineer”', 'https://apply.lambdaschool.com/courses/web-precourse/lessons/introduction-to-web-development-fundamentals/topic/learn-to-think-like-a-software-engineer/', 1);
+INSERT INTO objectives
+  (id, number, objective, url, lesson_id)
+  VALUES
+  (2, 2, 'Quiz: What does it mean to “think like a software engineer?”', 'https://apply.lambdaschool.com/courses/web-precourse/lessons/introduction-to-web-development-fundamentals/topic/quiz-what-does-it-mean-to-think-like-a-software-engineer/', 1);
--- server.js
+++ server.js
@@ -5,21 +5,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.json());
-app.use('/static', express.static('static'));
-app.set('view engine', 'hbs');
-
 const pgp = require('pg-promise')();
 const db = pgp({
-    host: 'localhost',
+    host: process.env.DB_HOST,
     port: 5432,
     database: process.env.DB_NAME,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD
 });
 
-app.get('/', function(req, res){
+app.use(bodyParser.json());
+app.use('/static', express.static('static'));
+app.set('view engine', 'hbs');
+
+
+// get homepage
+app.use('/', function(req, res){
   res.render('index');
+});
+
+// get all objectives
+app.get('/api/objectives', function(req, res){
+  db.any('SELECT * FROM objectives')
+  .then(data => res.json(data))
+  .catch(error => res.json({ error: error.message }));
 });
 
 app.listen(9090, function(){
