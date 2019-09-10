815da397bd2ddf3b4e7d8c3441d27e1743862c4d
mattc0llier <matt.collier@hotmail.com>
2019-09-10 10:52:49 +0100

    separated user and admin extension

---
+++ admin-extension/background.js
@@ -0,0 +1,137 @@
+
+// const client_id = 'mkhkokcinbeehocehnfenpgjfamooaib';
+// const redirectUri = "http://localhost:9090/leaderboard"
+// var auth_url = "https://localhost:9090/api/auth/account?client_id=" + client_id + "&redirect_uri=" + redirectUri + "&response_type=token";
+//
+// chrome.identity.launchWebAuthFlow({'url':auth_url,'interactive':true}, function(redirect_url){
+//     console.log(redirect_url)
+//   });
+
+
+
+const fetchActivities = (url) => {
+  return fetch(`http://localhost:9090/api/tabContext?url=${url}`, {
+    mode: "cors"
+  })
+  .then((response) => response.json())
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (!request.activity_complete) {
+    return
+  }
+
+  console.log('activity_complete');
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    fetchActivities(tabs[0].url)
+    .then(body => {
+      console.log('body before', body);
+      console.log('body[0].activity_id ', body[0][0].activity_id);
+     // to be triggered when completed objective click from content script
+      return fetch(`http://localhost:9090/api/activities/${body[0][0].activity_id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({ complete: true }),
+        headers: {
+           'Content-Type': 'application/json'
+         }
+       })
+       .then(function(response) {
+         return response.json();
+       })
+       .then(body => {
+         console.log('body', body);
+       })
+       .catch(error => console.log(error.message));
+     })
+   })
+});
+
+chrome.runtime.onConnect.addListener((port) => {
+  console.assert(port.name === "url_activities")
+
+  port.onMessage.addListener(function(msg) {
+    console.log('in content load listenr')
+
+    if (!msg.content_loaded) {
+      return
+    }
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+      const url = tabs[0].url
+      const urlIsLambdaSchool = url.includes("lambdaschool.com")
+
+      if (!urlIsLambdaSchool) return
+
+      fetchActivities(url)
+        .then((body) => {
+          console.log('sent activity')
+          port.postMessage({activity: body})
+        })
+    })
+  })
+ })
+
+ // on new opage
+ // get activities
+ // send message to content script
+
+   // send activity info to content.js.
+   // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+   //   console.log('tabs', tabs);
+   //   console.log('tabs[0].id', tabs[0].id);
+   //   console.log('activity: body', body);
+   //   chrome.tabs.sendMessage(tabs[0].id, {activity: body});
+   // });
+
+   //recieves message when item is marked complete
+
+ // update context on loading in a new page and send through response objective to content.js
+
+ //     fetch(`http://localhost:9090/api/tabContext?url=${tab.url}`, {
+ //        mode: "cors"
+ //      })
+ //      .then(function(response) {
+ //        return response.json();
+ //      })
+ //      .then(body => {
+ //        const completedActivityLister = function(request, sender, sendResponse) {
+ //            console.log('activity_complete');
+ //
+ //          // to be triggered when completed objective click from content script
+ //             return fetch(`http://localhost:9090/api/activities/${body[0].activity_id}`, {
+ //              method: 'PATCH',
+ //              body: JSON.stringify({ complete: true }),
+ //              headers: {
+ //                'Content-Type': 'application/json'
+ //              }
+ //            })
+ //            .then(function(response) {
+ //              return response.json();
+ //            })
+ //            .then(body => {
+ //              console.log('body', body);
+ //            })
+ //            .catch(error => console.log(error.message));
+ //        }
+ //
+ //        chrome.runtime.onMessage.removeListener(completedActivityLister)
+ //        console.log('body', body);
+ //        console.log('tab.id', tab.id);
+ //
+ //        // send activity info to content.js.
+ //        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+ //          console.log('tabs', tabs);
+ //          console.log('tabs[0].id', tabs[0].id);
+ //          console.log('activity: body', body);
+ //          chrome.tabs.sendMessage(tabs[0].id, {activity: body});
+ //        });
+ //
+ //        //recieves message when item is marked complete
+ //        chrome.runtime.onMessage.addListener(completedActivityLister);
+ //        console.log('completedActivityLister added')
+ //      })
+ //      .catch(error => console.log(error.message));
+ //    }
+ //  });
---
+++ admin-extension/popup.js
@@ -0,0 +1,165 @@
+const port = chrome.runtime.connect({name: "url_activities"});
+port.postMessage({content_loaded: true});
+port.onMessage.addListener(function(response) {
+  console.log('response popup', response);
+  console.log('activities popup', response.activity[0]);
+  console.log('activity popup', response.activity[0][0]);
+  console.log('topics popup', response.activity[1]);
+  const activity = response.activity[0][0]
+  const topics = response.activity[1]
+
+    //Start of popup tag search and form
+    const formNode = document.querySelector("form");
+    const activitiesNode = document.querySelector(".page-activities");
+    const searchTagsNode = document.querySelector("#searchTags");
+    const existingTagsNode = document.querySelector(".existing-tags");
+    const newTagsNode = document.querySelector(".new-tags");
+    const submitAllTagsButton = document.querySelector('#submit-all-tags');
+
+
+    //activities
+    const activityNode = document.createElement("div");
+    activityNode.className = "activity";
+    const existingActivity = `
+      <span style="display: ${activity.complete == false ? 'none' : 'block' }">✅</span>
+      <span style="display: ${activity.complete == true ? 'none' : 'block' }">🤔</span>
+      <p>${activity.objective}</p>
+    `;
+    activityNode.innerHTML = existingActivity
+    activitiesNode.appendChild(activityNode)
+
+    //tags
+    let oldTags = topics
+    let newTags = []
+    let searchTagArr = []
+
+    // onreceiving message add existing topics into old tags
+    if (!oldTags.length) {
+      const notifications = null
+      return notifications
+    } else {
+      const notifications = `
+        <ul class="topic-notfications">
+            ${topics.map(topic => `<li>${topic.topic}</li>`).join('')}
+         </ul>
+        `;
+      existingTagsNode.innerHTML = notifications
+    }
+
+
+    // serach all tags for topics that match what has been written in search
+    formNode.addEventListener("keyup", function(event) {
+      const inputText = document.querySelector(".text-area-input");
+      searchTagsNode.innerHTML = '';
+      // newTagsNode.removeChild(event.target.parentElement);
+      // fetch all tags that includes what has been written so far and filter for those that already match
+      fetch(`http://localhost:9090/api/tags/search?q=${inputText.value}%`, {
+        mode: "cors"
+      })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(body => {
+        console.log('body', body);
+        searchTagArr = body
+        const searchTags = `
+            ${searchTagArr.map(existingTags => `<option value="${existingTags.topic}" class="currentTag">`).join('')}
+        `;
+        searchTagsNode.innerHTML = searchTags
+      })
+      .catch(error => console.log(error.message));
+    });
+
+
+    // add a tag into the array to be be added to db
+    const submitTag = function(input) {
+      console.log('input submitTag', input);
+      // create tweet node
+      const tagNode = document.createElement("ul");
+      tagNode.className = "topic-notifications";
+
+      //push input into tag array
+      newTags.push(input)
+      console.log('new tag array', newTags);
+      tagNode.innerHTML = `<li>${input.topic}</li> `;
+      // create button node
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "x";
+      // appending delete button to tagNode
+      tagNode.appendChild(deleteButton);
+      //append tweet node to newTagsNode
+      newTagsNode.appendChild(tagNode);
+      // clears text area
+
+      //Setting event listener on delete button
+      deleteButton.addEventListener("click", function(event) {
+        let updatedTags = newTags.filter(word => word !== input)
+        console.log('updatedTags array - a.delete', updatedTags);
+        newTagsNode.removeChild(event.target.parentElement);
+
+      });
+    };
+
+    //add what is in the text box to newTagArr and see if it exist already
+    formNode.addEventListener("submit", function(event) {
+      event.preventDefault();
+      console.log('add tag hit');
+      const inputTextArea = document.querySelector(".text-area-input");
+      let inputTextValue = inputTextArea.value
+        // submit tag and tag_id from filterArr
+        //if filterArr is empty submit inputTex .value
+        console.log('new tag buttoon searchTagArr', searchTagArr);
+          console.log('new tag buttoon searchTagArr', inputTextValue);
+        const newTag = function(){
+          if (!searchTagArr.length) {
+            return {
+              tag_id: null,
+              topic: inputTextValue
+            }
+          } else {
+            return {
+              //just getting the first in array rather than actual tag object
+              tag_id: searchTagArr[0].tag_id,
+              topic: searchTagArr[0].topic
+            }
+          }
+        }
+        const newTagOutput = newTag()
+        console.log('newTagOutput', newTagOutput);
+        submitTag(newTagOutput);
+
+      inputTextArea.value = "";
+    });
+
+    // submit all new tags to the db
+    submitAllTagsButton.addEventListener("click", function(event) {
+      console.log('Submit all tags');
+      console.log('newTags - Submit all', newTags);
+
+      // Add the new tags arr to the db
+      const submitNewTagsToDb = function(newTags) {
+        console.log('submitNewTagsToDb hit');
+        console.log('activity.objective_id', activity.objective_id);
+        console.log('newTags', newTags);
+
+        fetch(`http://localhost:9090/api/tags`, {
+          method: 'POST',
+          body: JSON.stringify({
+            newTags: newTags,
+            objective_id: activity.objective_id
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(function(response) {
+          return response.json();
+        })
+        .then(body => {
+          newTags = []
+          console.log(body);
+        })
+      }
+      submitNewTagsToDb(newTags)
+    });
+})
