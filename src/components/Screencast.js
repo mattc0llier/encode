552d575ba9d52f142ff9ab17870fb17b4ea9f448
mattc0llier <matt.collier@hotmail.com>
2019-09-27 04:23:14 +0100

    change youtube link on screencast

--- src/components/Screencast.js
+++ src/components/Screencast.js
@@ -7,7 +7,7 @@ const Screencast = () => {
 return(
   <div className="screencast">
     <div className="table-center">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/gluSiyeUJ9E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://youtu.be/BrW97Ez3UX8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   </div>
   )
